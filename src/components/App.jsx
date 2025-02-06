b775823103312134baa67d01a0b66d00901930bb
María José <majosegarcia96@gmail.com>
2025-02-06 12:58:16 +0100

    modified search message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -67,7 +67,7 @@ function App() {
 
           <Filters filterName={filterName} setFilterName={setFilterName} filterHouse={filterHouse} setFilterHouse={setFilterHouse} filterGender={filterGender} setFilterGender={setFilterGender}/>
           
-          {filterCharacters.length !==0 ? <CharacterList characters={filterCharacters}/> : <><section className='section-error'><p className='error-msg'>No hay ningún personaje con el nombre {filterName}, de la casa {filterHouse} y género {filterGender}</p></section></>} 
+          {filterCharacters.length !==0 ? <CharacterList characters={filterCharacters}/> : <><section className='section-error'><p className='error-msg'>No se han encontrado resultados con esa búsqueda.</p></section></>} 
           
         </>
       }/>
